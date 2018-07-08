0edaf39d23a59db8b61b418cc58f36c9a2e0c12a
Justin Navarro <info@justinnavarro.net>
2018-07-08 16:40:22 -0400

    started building out the bottom section by adding icons and drawing out the track

--- lib/main.dart
+++ lib/main.dart
@@ -50,11 +50,10 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
 
           // Bottom Bar
-          new Container(
-            width: double.infinity,
-            height: 50.0,
-            color: Colors.grey,
-          )
+          new BottomBar(
+            cardCount: demoCards.length,
+            scrollPercent: 0.0,
+          ),
         ],
       ),
     );
@@ -313,5 +312,123 @@ class Card extends StatelessWidget {
         )
       ],
     );
+  }
+}
+
+class BottomBar extends StatelessWidget {
+  final int cardCount;
+  final double scrollPercent;
+
+  BottomBar({
+    this.cardCount,
+    this.scrollPercent,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 15.0, bottom: 15.0),
+      child: new Row(
+        children: <Widget>[
+          new Expanded(
+            child: new Center(
+              child: new Icon(
+                Icons.settings,
+                color: Colors.white,
+              ),
+            ),
+          ),
+          new Expanded(
+            child: new Container(
+              width: double.infinity,
+              height: 5.0,
+              child: new ScrollIndicator(
+                cardCount: cardCount,
+                scrollPercent: scrollPercent,
+              ),
+            ),
+          ),
+          new Expanded(
+            child: new Center(
+              child: new Icon(
+                Icons.add,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ScrollIndicator extends StatelessWidget {
+  final int cardCount;
+  final double scrollPercent;
+
+  ScrollIndicator({
+    this.cardCount,
+    this.scrollPercent,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return new CustomPaint(
+      painter: new ScrollIndicatorPainter(
+        cardCount: cardCount,
+        scrollPercent: scrollPercent,
+      ),
+      child: new Container(),
+    );
+  }
+}
+
+class ScrollIndicatorPainter extends CustomPainter {
+  final int cardCount;
+  final double scrollPercent;
+  final Paint trackPaint;
+  final Paint thumbPaint;
+
+  ScrollIndicatorPainter({
+    this.cardCount,
+    this.scrollPercent,
+  }) : trackPaint = new Paint()
+      ..color = const Color(0xFF444444)
+      ..style = PaintingStyle.fill,
+    thumbPaint = new Paint()
+      ..color = Colors.white
+      ..style = PaintingStyle.fill;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    canvas.drawRRect(
+      new RRect.fromRectAndCorners(
+        new Rect.fromLTWH(0.0, 0.0, size.width, size.height),
+        topLeft: new Radius.circular(3.0),
+        topRight: new Radius.circular(3.0),
+        bottomLeft: new Radius.circular(3.0),
+        bottomRight: new Radius.circular(3.0),
+      ),
+      trackPaint
+    );
+
+    final thumbWidth = size.width / cardCount;
+    final thumbLeft = scrollPercent * size.width;
+
+    canvas.drawRRect(
+        new RRect.fromRectAndCorners(
+          new Rect.fromLTWH(thumbLeft, 0.0, thumbWidth, size.height),
+          topLeft: new Radius.circular(3.0),
+          topRight: new Radius.circular(3.0),
+          bottomLeft: new Radius.circular(3.0),
+          bottomRight: new Radius.circular(3.0),
+        ),
+        thumbPaint
+    );
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
   }
 }
